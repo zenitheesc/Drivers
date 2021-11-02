a5b785dc791dfb1b3a73084d1f9eca0cc2ed812c
Carlos Henrique Craveiro Aquino Veras <carlos.craveiro@usp.br>
2021-11-02 05:42:24 -0300

    feature: adds the "builderIdNdMask" feature
    Adds the builderIdNdMask structure and functions to the header of the
    library.
    
    Signed-off-by: Carlos Veras <carlos.craveiro@usp.br>

--- CAN/CANZenTool.h
+++ CAN/CANZenTool.h
@@ -10,7 +10,79 @@
  * */
 
 #include "stm32f1xx_hal_can.h"
+
 #include <stdbool.h>
+
+/* Declaration of the Structure acts as a builder for the Mask and Id for @ref CANZenTool_setFilter*/
+typedef struct CANZenTool_IdNdMaskBuilder_t CANZenTool_IdNdMaskBuilder_t;
+
+/* Definition of the Structure (Proto-Class) acts as a builder for the Mask and Id for @ref CANZenTool_setFilter*/
+struct CANZenTool_IdNdMaskBuilder_t {
+    
+    unsigned int m_rawIds;
+
+    uint32_t m_idBuffer;
+    uint32_t m_maskBuffer;
+
+    void (*addId)(CANZenTool_IdNdMaskBuilder_t*, uint32_t);
+    uint32_t (*getResultId)(CANZenTool_IdNdMaskBuilder_t*);
+    uint32_t (*getResultMask)(CANZenTool_IdNdMaskBuilder_t*);
+    void (*addIdList)(CANZenTool_IdNdMaskBuilder_t*, uint32_t[]);
+};
+
+
+/**
+ * @brief   "adds" a standard CAN Id to the "builder" refered as "this". The addition "re-calculates"
+ *          the new Id and Mask inside the "builder" structure based in the income Id.
+ *          Note: this function can be both used as a "method" of the "builder" structure 
+ *          or as a stand-alone function.
+ *
+ * @param   this pointer to a CANZenTool_IdNdMaskBuilder_t structure (proto-class). 
+ *
+ * @param   newId unsigned integer of 32 bits that represents a standard CAN Id following a Big-endian convention.
+ */
+void CANZenTool_addId(CANZenTool_IdNdMaskBuilder_t* this, uint32_t newId);
+
+
+/**
+ * @brief   "adds" a list of standard CAN Ids to the "builder" refered as "this". 
+ *          The addition "re-calculates" the new Id and Mask inside the "builder" structure 
+ *          based in the income Ids. Note: this function can be both used as a "method" of 
+ *          the "builder" structure or as a stand-alone function.
+ *
+ * @param   idListLength unsigned integer of 32 bits that represents the length of the income idList. 
+ *
+ * @param   idList (pointer to uint32_t) array of standard CAN Ids following the Big-endian convention. 
+ */
+void CANZenTool_addIdList(CANZenTool_IdNdMaskBuilder_t* this,  uint32_t idListLength, uint32_t idList[]);
+
+
+/**
+ * @brief   gets the result Mask constructed in the "builder" structure (proto-class).
+ *
+ * @param   this pointer to a CANZenTool_IdNdMaskBuilder_t structure (proto-class). 
+ *
+ * @return  the result Mask (unsigned int of 32 bits) constructed in the "builder" structure.
+ */
+uint32_t CANZenTool_getResultMask(CANZenTool_IdNdMaskBuilder_t* this);
+
+
+/**
+ * @brief   gets the result Id constructed in the "builder" structure (proto-class).
+ *
+ * @param   this pointer to a CANZenTool_IdNdMaskBuilder_t structure (proto-class).
+ *
+ * @return  the result Id (unsigned int of 32 bits) constructed in the "builder" structure.
+ */
+uint32_t CANZenTool_getResultId(CANZenTool_IdNdMaskBuilder_t* this);
+
+
+/**
+ * @brief   creates a new "builder" structure (proto-class) of type @ref CANZenTool_IdNdMaskBuilder_t.
+ *
+ * @return  a new "builder" structure (proto-class) @ref CANZenTool_IdNdMaskBuilder_t.
+ */
+CANZenTool_IdNdMaskBuilder_t CANZenTool_newFilterBuilder();
 
 
 /**
@@ -58,7 +130,7 @@ void CANZenTool_setFilter(CAN_HandleTypeDef* hcan, CAN_FilterTypeDef* canFilterC
  *
  * @return 	The writed CAN Frame.
  */
- CAN_TxHeaderTypeDef CANZenTool_writeStdCanFrame(uint32_t dlc, uint32_t id, bool isData);
+CAN_TxHeaderTypeDef CANZenTool_writeStdCanFrame(uint32_t dlc, uint32_t id, bool isData);
 
 
 /**
