91c76e7a8b9b0d6713ac18abf5b6c36ef5aa5e09
Leonardo Celente <leocelente@gmail.com>
2022-04-13 18:50:48 -0300

    Blind change to platform.h

--- INA3221/INA3221.c
+++ INA3221/INA3221.c
@@ -7,11 +7,11 @@
 static result_uint16_t read(ina3221_t ina, uint8_t addr) {
   uint8_t tx = addr;
   buffer_view_t tx_v = {.size = sizeof(tx), .data = &tx};
-  error_t e = i2c_transmit(ina.device, tx);
+  error_t e = i2c_transmit(ina.device, tx_v);
 
   uint8_t rx[2] = {0};
   buffer_view_t rx_v = {.size = sizeof(rx), .data = rx};
-  e |= i2c_receive(ina.device, rx);
+  e |= i2c_receive(ina.device, rx_v);
 
   uint16_t value = (rx[1] << 8) | (rx[0]);
   result_uint16_t res = {.hasError = e, .value = value};
@@ -20,151 +20,146 @@ static result_uint16_t read(ina3221_t ina, uint8_t addr) {
 
 static error_t write(ina3221_t ina, uint8_t addr, uint16_t value) {
   uint8_t tx[] = {addr, value << 8, value};
-  buffer_view_t view = {.size = sizeof(tx), .data = tx};
-  return i2c_transmit(ina.device, tx);
+  buffer_view_t tx_v = {.size = sizeof(tx), .data = tx};
+  return i2c_transmit(ina.device, tx_v);
 }
 
 static int16_t to_value(uint16_t raw_value) {
 
-	raw_value >>= 3;
-	if (raw_value & (1 << (15 - 3))) {
-		raw_value |= 0xE000;
-	}
-	return (int16_t) raw_value;
+  raw_value >>= 3;
+  if (raw_value & (1 << (15 - 3))) {
+    raw_value |= 0xE000;
+  }
+  return (int16_t)raw_value;
 }
 
 error_t ina3221_init(ina3221_t ina) {
-    if(!ina3221_alive(ina)){
-    	return 1;
-    }
+  if (!ina3221_alive(ina)) {
+    return 1;
+  }
 
-	uint8_t address_config = CONFIG_ADR;
-	uint16_t bit_config = 0x7127 | RESET_CONFIG;
+  uint8_t address_config = CONFIG_ADR;
+  uint16_t bit_config = 0x7127 | RESET_CONFIG;
 
-    result16_t id = i2c_read16(ina.device, CONFIG_ADR);
+  result_uint16_t id = read(ina, CONFIG_ADR);
 
-    delay_ms(1);
+  delay_ms(1);
 
-    if (id.hasError) {
-        return id.hasError;
-    }
+  if (id.hasError) {
+    return id.hasError;
+  }
 
-    delay_ms(10);
+  delay_ms(10);
 
-	return 0;
-    return i2c_write16(ina.device, address_config, bit_config);
+  return 0;
+  return write(ina, address_config, bit_config);
 }
 
 error_t ina3221_config(ina3221_t ina) {
 
-    uint16_t config_register = 0;
-    uint8_t address_config = CONFIG_ADR;
+  uint16_t config_register = 0;
+  uint8_t address_config = CONFIG_ADR;
 
-    config_register |= (ina.config.ch1 << BIT_CH1);
-    config_register |= (ina.config.ch2 << BIT_CH2);
-    config_register |= (ina.config.ch3 << BIT_CH3);
-    config_register |= (ina.config.avg_mode << BIT_AVG);
-    config_register |= (ina.config.v_bus_ct << BIT_BUS_CT);
-    config_register |= (ina.config.v_sh_ct << BIT_SH_CT);
-    config_register |= (ina.config.op_mode << BIT_MODE);
+  config_register |= (ina.config.ch1 << BIT_CH1);
+  config_register |= (ina.config.ch2 << BIT_CH2);
+  config_register |= (ina.config.ch3 << BIT_CH3);
+  config_register |= (ina.config.avg_mode << BIT_AVG);
+  config_register |= (ina.config.v_bus_ct << BIT_BUS_CT);
+  config_register |= (ina.config.v_sh_ct << BIT_SH_CT);
+  config_register |= (ina.config.op_mode << BIT_MODE);
 
-    return 0;
-    return i2c_write16(ina.device, address_config, config_register);;
+  return 0;
+  return write(ina, address_config, config_register);
 }
 
-error_t ina3221_mensurement(ina3221_t ina, ina3221_values_t *values){
-    float final_value;
+error_t ina3221_mensurement(ina3221_t ina, ina3221_values_t *values) {
+  float final_value;
 
-    // Shunt Voltage values
+  // Shunt Voltage values
 
-    result16_t raw = i2c_read16(ina.device, CH1_SHUNT_V);
-    if (raw.hasError) {
-        return raw.hasError;
-    }
+  result_uint16_t raw = read(ina, CH1_SHUNT_V);
+  if (raw.hasError) {
+    return raw.hasError;
+  }
 
-	final_value = T_SH_CONV * to_value(raw.value);
+  final_value = T_SH_CONV * to_value(raw.value);
 
-    values->ch1_sh_v = final_value;
+  values->ch1_sh_v = final_value;
 
+  raw = read(ina, CH2_SHUNT_V);
+  if (raw.hasError) {
+    return raw.hasError;
+  }
 
-    raw = i2c_read16(ina.device, CH2_SHUNT_V);
-    if (raw.hasError) {
-        return raw.hasError;
-    }
+  final_value = T_SH_CONV * to_value(raw.value);
 
-	final_value = T_SH_CONV * to_value(raw.value);
+  values->ch2_sh_v = final_value;
 
-    values->ch2_sh_v = final_value;
+  raw = read(ina, CH3_SHUNT_V);
+  if (raw.hasError) {
+    return raw.hasError;
+  }
 
+  final_value = T_SH_CONV * to_value(raw.value);
 
-    raw = i2c_read16(ina.device, CH3_SHUNT_V);
-    if (raw.hasError) {
-        return raw.hasError;
-    }
+  values->ch3_sh_v = final_value;
 
-	final_value = T_SH_CONV * to_value(raw.value);
+  // Bus Voltage Values
 
-    values->ch3_sh_v = final_value;
+  raw = read(ina, CH1_BUS_V);
+  if (raw.hasError) {
+    return raw.hasError;
+  }
+  final_value = T_BUS_CONV * to_value(raw.value);
 
+  values->ch1_bus_v = final_value;
 
-    // Bus Voltage Values
+  raw = read(ina, CH2_BUS_V);
+  if (raw.hasError) {
+    return raw.hasError;
+  }
+  final_value = T_BUS_CONV * to_value(raw.value);
 
-    raw = i2c_read16(ina.device, CH1_BUS_V);
-    if (raw.hasError) {
-        return raw.hasError;
-    }
-	final_value = T_BUS_CONV * to_value(raw.value);
+  values->ch2_bus_v = final_value;
 
-    values->ch1_bus_v = final_value;
+  raw = read(ina, CH3_BUS_V);
+  if (raw.hasError) {
+    return raw.hasError;
+  }
+  final_value = T_BUS_CONV * to_value(raw.value);
 
+  values->ch3_bus_v = final_value;
 
-    raw = i2c_read16(ina.device, CH2_BUS_V);
-    if (raw.hasError) {
-        return raw.hasError;
-    }
-	final_value = T_BUS_CONV * to_value(raw.value);
+  // Converted Current values   U(shunt) = R * i
 
-    values->ch2_bus_v = final_value;
+  float CH1_current = (values->ch1_sh_v) / (ina.config.ch1_resistor);
+  float CH2_current = (values->ch2_sh_v) / (ina.config.ch2_resistor);
+  float CH3_current = (values->ch3_sh_v) / (ina.config.ch3_resistor);
 
+  values->ch1_current = CH1_current;
+  values->ch2_current = CH2_current;
+  values->ch3_current = CH3_current;
 
-    raw = i2c_read16(ina.device, CH3_BUS_V);
-    if (raw.hasError) {
-        return raw.hasError;
-    }
-	final_value = T_BUS_CONV * to_value(raw.value);
+  // Power values   (potência do bus - potência do shunt)
 
-    values->ch3_bus_v = final_value;
+  float CH1_pot = ((values->ch1_bus_v) * (values->ch1_current)) -
+                  ((values->ch1_sh_v) * (values->ch1_current));
+  float CH2_pot = ((values->ch2_bus_v) * (values->ch2_current)) -
+                  ((values->ch2_sh_v) * (values->ch2_current));
+  float CH3_pot = ((values->ch3_bus_v) * (values->ch3_current)) -
+                  ((values->ch3_sh_v) * (values->ch3_current));
 
-    // Converted Current values   U(shunt) = R * i
+  values->ch1_pot = CH1_pot;
+  values->ch2_pot = CH2_pot;
+  values->ch3_pot = CH3_pot;
 
-	float CH1_current = (values->ch1_sh_v) / (ina.config.ch1_resistor);
-	float CH2_current = (values->ch2_sh_v) / (ina.config.ch2_resistor);
-	float CH3_current = (values->ch3_sh_v) / (ina.config.ch3_resistor);
-
-    values->ch1_current = CH1_current;
-    values->ch2_current = CH2_current;
-    values->ch3_current = CH3_current;
-
-	// Power values   (potência do bus - potência do shunt)
-
-	float CH1_pot = ((values->ch1_bus_v) * (values->ch1_current))
-			- ((values->ch1_sh_v) * (values->ch1_current));
-	float CH2_pot = ((values->ch2_bus_v) * (values->ch2_current))
-			- ((values->ch2_sh_v) * (values->ch2_current));
-	float CH3_pot = ((values->ch3_bus_v) * (values->ch3_current))
-			- ((values->ch3_sh_v) * (values->ch3_current));
-
-    values->ch1_pot = CH1_pot;
-    values->ch2_pot = CH2_pot;
-    values->ch3_pot = CH3_pot;
-
-	return 0;
+  return 0;
 }
 
 bool ina3221_alive(ina3221_t ina) {
-    result16_t code = i2c_read16(ina.device, DIE_ADR);
-    if (code.value != DIE_CODE){
-        return false;
-    }
-    return true;
+  result_uint16_t code = read(ina, DIE_ADR);
+  if (code.value != DIE_CODE) {
+    return false;
+  }
+  return true;
 }
--- INA3221/INA3221.h
+++ INA3221/INA3221.h
@@ -2,37 +2,37 @@
 // Created by gustavo on 05/09/2021.
 //
 
-
 #ifndef OBJ_C_INA3221_H
 #define OBJ_C_INA3221_H
 
 #include "platform/platform.h"
 #include <stdbool.h>
+#include <stdint.h>
 
 //      REGISTER_NAME     ADDRESS(HEX)
-#define CONFIG_ADR            0x00
-#define CH1_SHUNT_V           0x01
-#define CH1_BUS_V             0x02
-#define CH2_SHUNT_V           0x03
-#define CH2_BUS_V             0x04
-#define CH3_SHUNT_V           0x05
-#define CH3_BUS_V             0x06
-#define DIE_ADR               0xFF
+#define CONFIG_ADR 0x00
+#define CH1_SHUNT_V 0x01
+#define CH1_BUS_V 0x02
+#define CH2_SHUNT_V 0x03
+#define CH2_BUS_V 0x04
+#define CH3_SHUNT_V 0x05
+#define CH3_BUS_V 0x06
+#define DIE_ADR 0xFF
 
 // CONST
 
-#define BIT_CH1              14
-#define BIT_CH2              13
-#define BIT_CH3              12
-#define BIT_AVG              9
-#define BIT_BUS_CT           6
-#define BIT_SH_CT            3
-#define BIT_MODE             0
-#define DIE_CODE             0x3220
-#define RESET_CONFIG         (1<<15)
+#define BIT_CH1 14
+#define BIT_CH2 13
+#define BIT_CH3 12
+#define BIT_AVG 9
+#define BIT_BUS_CT 6
+#define BIT_SH_CT 3
+#define BIT_MODE 0
+#define DIE_CODE 0x3220
+#define RESET_CONFIG (1 << 15)
 
-#define T_SH_CONV             40e-6f
-#define T_BUS_CONV            8e-3f
+#define T_SH_CONV 40e-6f
+#define T_BUS_CONV 8e-3f
 
 typedef struct {
 
@@ -63,38 +63,35 @@ typedef struct {
 	float ch2_resistor;
 	float ch3_resistor;
 
-
 } ina3221_config_t;
-
 
 typedef struct {
 
-    float ch1_sh_v; // sh_v = R * (i)
-    float ch1_bus_v;
-    float ch2_sh_v;
-    float ch2_bus_v;
-    float ch3_sh_v;
-    float ch3_bus_v;
+	float ch1_sh_v; // sh_v = R * (i)
+	float ch1_bus_v;
+	float ch2_sh_v;
+	float ch2_bus_v;
+	float ch3_sh_v;
+	float ch3_bus_v;
 
-    float ch1_current;
-    float ch2_current;
-    float ch3_current;
+	float ch1_current;
+	float ch2_current;
+	float ch3_current;
 
-    uint16_t ch1_resistor;
-    uint16_t ch2_resistor;
-    uint16_t ch3_resistor;
+	uint16_t ch1_resistor;
+	uint16_t ch2_resistor;
+	uint16_t ch3_resistor;
 
-    float ch1_pot;
-    float ch2_pot;
-    float ch3_pot;
+	float ch1_pot;
+	float ch2_pot;
+	float ch3_pot;
 
 } ina3221_values_t;
 
-
 typedef struct {
 
-    i2c_device_t device;
-    ina3221_config_t config;
+	i2c_device_t device;
+	ina3221_config_t config;
 
 } ina3221_t;
 
@@ -103,6 +100,4 @@ error_t ina3221_config(ina3221_t ina);
 error_t ina3221_mensurement(ina3221_t ina, ina3221_values_t *values);
 bool ina3221_alive(ina3221_t ina);
 
-
-
-#endif //OBJ_C_INA3221_H
+#endif // OBJ_C_INA3221_H
