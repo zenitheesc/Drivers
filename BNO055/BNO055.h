8aeff94a2018ebd24f3030c740f96d774af433fa
murilopasini <murilopasini@usp.br>
2022-06-10 10:14:37 -0300

    creating .H file #31

---
+++ BNO055/BNO055.h
@@ -0,0 +1,43 @@
+/*
+ * BNO055.h
+ *
+ *  Created on: Jun 10, 2022
+ *      Author: Murilo Henrique Pasini Trevisan
+*/
+
+#ifndef INC_BNO055_H
+#define INC_BNO055_H
+
+#include "platform.h"
+
+/* REGISTRADORES */
+
+// Name                //Address(hex)
+
+// Registradores de configuração
+
+
+
+
+
+
+
+
+
+// Registradores de leitura dos dados
+// Acelerômetro
+
+// Temperatura
+
+// Giroscópio
+
+// Magnetômetro
+
+// Euler degree
+
+// Quaternions degree
+
+// LIA(linear acceleration)
+
+// GV(Gravity vector)
+
