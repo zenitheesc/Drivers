20a3293b0fb5bb60c7fb0aecd181dc44a5608ff9
murilopasini <murilopasini@usp.br>
2022-06-24 18:01:23 -0300

    utilização de ponteiros

--- BNO055/BNO055.c
+++ BNO055/BNO055.c
@@ -15,11 +15,11 @@ static result_uint8_t read(BNO055_t bno, uint8_t addr){
         result_uint8_t result = { 0 };
         //addres send
         buffer_view_t addr_tr = { .data = &addr, .size = sizeof(addr)};
-        result.HasError |= i2c_transmit(bno.dev, addr_tr);
+        result.HasError |= i2c_transmit(bno->dev, addr_tr);
         //value send
         uint8_t rx = 0;
         buffer_view_t read_tr = {.data = &rx, .size = sizeof(rx)};
-        result.HasError |= i2c_receive(bno.dev,read_tr);
+        result.HasError |= i2c_receive(bno->dev,read_tr);
         result.value = rx;
         return result;
 }
@@ -29,5 +29,7 @@ static error_t write(BNO055_t bno, uint8_t addr, uint16_t value){
         //shifting value to 8 bit i2c
         uint8_t data[] = {addr, value};
         buffer_view_t tx = {.data = data, .size = sizeof(data)};
-        return i2c_transmit(bno.dev, tx);
+        return i2c_transmit(bno->dev, tx);
 }
+
+
